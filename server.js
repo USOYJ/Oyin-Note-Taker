323111cc06b4c701a8121b45712e532ae1b5654d
Oyin <125850331+USOYJ@users.noreply.github.com>
2023-08-14 17:35:16 -0400

    javascript

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const { v4: uuidv4 } = require("uuid");
 
 const allNotes = require('./db/db.json');
